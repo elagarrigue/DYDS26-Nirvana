f0f3ecd61d3b6cc92c47b8a3922de4346fa4df0d
tombertotto <tom.bertotto@gmail.com>
2026-04-29 19:55:00 -0300

    G6: separacion de RemoteMovie a RemoteMovie y RemoteResult en modulos diferentes

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/remote/RemoteMovie.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/remote/RemoteMovie.kt
@@ -4,14 +4,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RemoteResult(
-    val page: Int,
-    val results: List<RemoteMovie>,
-    @SerialName("total_pages") val totalPages: Int,
-    @SerialName("total_results") val totalResults: Int
-)
-
-@Serializable
 data class RemoteMovie(
     val id: Int,
     val title: String,
